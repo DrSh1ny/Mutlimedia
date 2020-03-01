83e0cbf9cc7ac034f378175cbfd17e8389ab76d9
DrSh1ny <DrSh1ny@users.noreply.github.com>
2020-03-01 16:38:47 +0000

    Experimental

---
+++ Experimental/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+(function()
+{
+	//automatically called as soon as the javascript is loaded
+	window.addEventListener("load", main);
+}());
+
+
+function main()
+{
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	var cw = canvas.width;
+	var ch = canvas.height;
+
+	
+
+}
